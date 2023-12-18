86e0387bf9d301022029aecbbb8c0e6c86b8806e
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-18 13:02:45 +0600

    set header menu section

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/shared/Footer/Footer';
+import Header from '../pages/shared/Header/Header';
 
 const MainLayout = () => {
     return (
         <div>
-            
+            <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
