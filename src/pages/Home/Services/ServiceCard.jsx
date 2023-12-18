49f7689ff2ccc6c47f9fc039809f18686b210a3a
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-18 17:36:32 +0600

    display all card data of services section

---
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ServiceCard = ({service}) => {
+    const {title,img,price} = service;
+    return (
+        <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src={img} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body  text-center">
+    <h2 className="card-title">{title}</h2>
+    <p>Price: ${price} </p>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default ServiceCard;
