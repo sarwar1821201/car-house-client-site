1c89a94e82583aa213678205731cdd02e9324e70
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-18 20:52:11 +0600

    create firebase intrigation

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home/Home";
 import MainLayout from "../layout/MainLayout";
+import Login from "../pages/Authenticate/Login/Login";
 
 
 
@@ -12,6 +13,10 @@ const router = createBrowserRouter ([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element:  <Login></Login>
             }
         ]
     }
