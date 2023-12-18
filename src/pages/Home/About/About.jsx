9fa1f3e64c61350304c45575f170857deda1ac9e
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-18 16:34:03 +0600

    create about section with image relative position

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import person from '../../../assets/images/about_us/person.jpg'
+import parts from '../../../assets/images/about_us/parts.jpg'
 
 const About = () => {
     return (
         <div>
-            
+<div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row">
+  
+    <div className='lg:w-1/2 relative ' >
+    <img src={person} className="w-3/4 rounded-lg shadow-2xl" />
+    <img src={parts} className="w-1/2 absolute right-5 top-1/2 border-8 border-white  rounded-lg shadow-2xl" />
+
+
+    </div>
+
+    <div className='lg:w-1/2' >
+        <h3 className='text-3xl text-orange-500 font-bold' >About Us</h3>
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get More Info </button>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import About from '../About/About';
 
 const Home = () => {
     return (
         <div>
             {/* <h2>This is Home</h2> */}
             <Banner></Banner>
+            <About></About>
         </div>
     );
 };
