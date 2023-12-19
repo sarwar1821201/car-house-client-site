b005521b69d1209ac622e187d5c9d2166b6385b7
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-19 13:47:37 +0600

    create checkout form

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ServiceCard = ({service}) => {
-    const {title,img,price} = service;
+    const {title, _id, img,price} = service;
     return (
         <div>
             <div className="card w-96 bg-base-100 shadow-xl">
@@ -12,7 +13,9 @@ const ServiceCard = ({service}) => {
     <h2 className="card-title">{title}</h2>
     <p>Price: ${price} </p>
     <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
+     <Link to={`/checkout/${_id}`} > 
+     <button className="btn btn-primary">Book Now</button>
+     </Link>
     </div>
   </div>
 </div>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/Home/Home/Home";
 import MainLayout from "../layout/MainLayout";
 import Login from "../pages/Authenticate/Login/Login";
 import Register from "../pages/Authenticate/Login/Register/Register";
+import CheckOut from "../pages/CheckOut/CheckOut";
 
 
 
@@ -22,6 +23,11 @@ const router = createBrowserRouter ([
             {
                 path: '/register',
                 element: <Register></Register>
+            },
+            {
+                path: '/checkout/:id',
+                element: <CheckOut></CheckOut>,
+                loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
             }
         ]
     }
