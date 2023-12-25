0177384e5aff01416b9951a9437acb59545dff23
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-25 13:50:25 +0600

    verify and decode jwt token

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -9,7 +9,12 @@ const Bookings = () => {
     const url= `http://localhost:5000/bookings?email=${user?.email}`
 
     useEffect( ()=>{
-        fetch(url)
+        fetch(url , {
+            method: 'GET',
+            headers: {
+              authorization : ` Bearer ${localStorage.getItem ('car-access-token') } `
+            }
+        }  )
         .then(res=> res.json() )
         .then(data=> {
             setBookings (data)
